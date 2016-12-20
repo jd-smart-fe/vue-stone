982629c1b7487118b70fe89e945497a0b6fbe9c2
hucong <hucong@jd.com>
2016-12-20 11:51:07 +0800

    add an route, time page

---
+++ src/Time.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <div class="">
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="css">
+</style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,28 @@
 import Vue from 'vue';
 import App from './App';
+import Time from './Time';
 import Store from './store';
 
 Store.install(Vue);
+
+const routes = {
+  '/': App,
+  '/time': Time,
+};
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
-  components: { App },
+  data: {
+    currentRoute: window.location.pathname,
+  },
+  computed: {
+    ViewComponent() {
+      return routes[this.currentRoute];
+    },
+  },
+  render(h) {
+    return h(this.ViewComponent);
+  },
 });
