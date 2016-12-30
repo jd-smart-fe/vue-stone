c330507aa53a145e87cd50c89aec29015425457f
hucong <hucong@jd.com>
2016-12-30 20:45:29 +0800

    babel default.js

--- src/units/units.js
+++ src/units/units.js
@@ -4,20 +4,20 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-function timeTaskExpress() {
+function taskTimeExpress() {
   var min = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '*';
   var hour = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '*';
-  var day = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : '*';
+  var date = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : '*';
   var month = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : '*';
   var week = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : '*';
   var year = arguments.length > 5 && arguments[5] !== undefined ? arguments[5] : '*';
 
   var weekStr = Array.isArray(week) ? week.join(',') : week;
-  var express = min + '_' + hour + '_' + day + '_' + month + '_' + weekStr + '_' + year;
+  var express = min + '_' + hour + '_' + date + '_' + month + '_' + weekStr + '_' + year;
   return express;
 }
 
-function textTimeTaskExpress(express) {
+function textTaskTimeExpress(express) {
   var arr = express.split('_');
 
   if (arr[5] !== '*') {
@@ -27,16 +27,15 @@ function textTimeTaskExpress(express) {
   if (arr[4] === '*' && arr[3] === '*' && arr[2] === '*') {
     return '每天';
   }
-
   var week = arr[4].split(',');
 
   return weekArrToStr(week);
 }
 
-function arrayTimeTaskExpress(express) {
+function arrayTaskTimeExpress(express) {
   var week = express.split('_')[4];
   if (week === '*') {
-    return new Date().getDay() === 0 ? [7] : [new Date().getDay()];
+    return [];
   }
   var arr = week.split(',').map(function (val) {
     return val * 1;
@@ -46,27 +45,29 @@ function arrayTimeTaskExpress(express) {
 
 function weekArrToStr(week) {
   var text = [];
+
   week.forEach(function (val) {
-    switch (val) {
-      case val === '1' || 1:
+    var num = val * 1;
+    switch (true) {
+      case num === 1:
         text.push('周一');
         break;
-      case val === '2' || 2:
+      case num === 2:
         text.push('周二');
         break;
-      case val === '3' || 3:
+      case num === 3:
         text.push('周三');
         break;
-      case val === '4' || 4:
+      case num === 4:
         text.push('周四');
         break;
-      case val === '5' || 5:
+      case num === 5:
         text.push('周五');
         break;
-      case val === '6' || 6:
+      case num === 6:
         text.push('周六');
         break;
-      case val === '7' || 7:
+      case num === 7:
         text.push('周日');
         break;
       default:
@@ -87,12 +88,36 @@ function weekArrToStr(week) {
   return text.join('、');
 }
 
-exports.default = {
-  arrayTimeTaskExpress: arrayTimeTaskExpress,
+function resolveTimeInTasklist(date) {
+  return date.split(' ')[1];
+}
 
-  textTimeTaskExpress: textTimeTaskExpress,
+function toTasklistStr(obj) {
+  var express = obj.task_time_express;
+  var minutes = obj.next_left_minutes;
+  var str1 = textTaskTimeExpress(express);
+
+  var hour = Math.floor(minutes / 60);
+  var min = minutes - hour * 60;
+  var day = Math.floor(hour / 24);
+
+  if (day >= 1) {
+    return str1 + ' ' + day + '\u5929\u540E\u6267\u884C';
+  }
+
+  return str1 + ' ' + hour + '\u5C0F\u65F6' + min + '\u5206\u949F\u540E\u6267\u884C';
+}
+
+exports.default = {
+  arrayTaskTimeExpress: arrayTaskTimeExpress,
+
+  textTaskTimeExpress: textTaskTimeExpress,
 
   weekArrToStr: weekArrToStr,
 
-  timeTaskExpress: timeTaskExpress
+  taskTimeExpress: taskTimeExpress,
+
+  resolveTimeInTasklist: resolveTimeInTasklist,
+
+  toTasklistStr: toTasklistStr
 };
