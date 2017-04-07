c15109999505ffa6f58b7289d721a8192151ba7f
hucong <hucong@jd.com>
2017-04-07 10:59:50 +0800

    添加actionsBox组件

---
+++ src/components/actions-box.vue
@@ -0,0 +1,72 @@
+<template lang="html">
+  <div class="">
+    <div :class='["c-actions", value ? "c-actions-active" : "" ]'>
+
+    </div>
+
+    <transition name="fade">
+      <v-mask v-show="value" :shown="mask_shown"></v-mask>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'v-actions-box',
+
+  data() {
+    return {
+      mask_shown: this.value,
+    };
+  },
+
+  computed: {
+  },
+
+  props: {
+    value: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+
+  watch: {
+    value(val) {
+      if (val) {
+
+        this.mask_shown = true;
+      } else {
+
+        // 300ms 是为了能够触发mask消失时的过渡动画
+        setTimeout(() => {
+          this.mask_shown = false;
+        }, 300);
+      }
+    },
+  },
+};
+</script>
+
+<style lang="css">
+  .c-actions{
+    position: fixed;
+    top: 100%;
+    left: 0;
+    right: 0;
+    margin: auto;
+
+    text-align: center;
+    font-size: 18px;
+
+    width: 3.1rem;
+
+    z-index: 101;
+
+    transition: transform .3s ease-out;
+
+    &.c-actions-active{
+      transform: translate(0, -100%);
+    }
+  }
+</style>
