9bab6a70b094922af40af0fcfb56948376a33ad4
yleo77 <ylep77@gmail.com>
2017-04-14 11:54:34 +0800

    add mixin css to utils.css and example

--- src/main.js
+++ src/main.js
@@ -5,50 +5,56 @@ import App from './App';
 import Store from './store';
 
 import './pages/_example_style';
-import _tmpl_back from './pages/_tmpl_back';
+import TmplBack from './pages/_tmpl_back';
 
-import Timer from './pages/timer';
-import NotFind from './pages/404';
-import Button from './pages/button';
-import Range from './pages/range';
-import Panel from './pages/panel';
-import Icons from './pages/icons';
-import DaysPicker from './pages/days-picker';
-import Modes from './pages/modes';
-import Slider from './pages/slider';
-import ActionSheet from './pages/action-sheet';
-import Dialog from './pages/dialog';
-import Form from './pages/form';
-import Counter from './pages/counter';
-import Picker from './pages/picker';
-import Switch from './pages/switch';
-import Transition from './pages/transition';
-
+const list = [{
+  name: 'timer',
+}, {
+  name: '404',
+}, {
+  name: 'button',
+}, {
+  name: 'range',
+}, {
+  name: 'panel',
+}, {
+  name: 'icons',
+}, {
+  name: 'days-picker',
+}, {
+  name: 'modes',
+}, {
+  name: 'slider',
+}, {
+  name: 'action-sheet',
+}, {
+  name: 'dialog',
+}, {
+  name: 'form',
+}, {
+  name: 'counter',
+}, {
+  name: 'picker',
+}, {
+  name: 'switch',
+}, {
+  name: 'transition',
+}];
 
 Vue.use(VueRouter);
 Store.install(Vue);
 
-Vue.component('tmpl-back', _tmpl_back);
+Vue.component('tmpl-back', TmplBack);
 
-const routes = [
-  { path: '/', component: App },
-  { path: '/timer', component: Timer },
-  { path: '/404', component: NotFind },
-  { path: '/button', component: Button },
-  { path: '/range', component: Range },
-  { path: '/panel', component: Panel },
-  { path: '/icons', component: Icons },
-  { path: '/days-picker', component: DaysPicker },
-  { path: '/modes', component: Modes },
-  { path: '/timer', component: Timer },
-  { path: '/slider', component: Slider },
-  { path: '/action-sheet', component: ActionSheet },
-  { path: '/dialog', component: Dialog },
-  { path: '/form', component: Form },
-  { path: '/counter', component: Counter },
-  { path: '/picker', component: Picker },
-  { path: '/switch', component: Switch },
-  { path: '/transition', component: Transition },
+const routes = [{ path: '/', component: App },
+  /* eslint-disable global-require */
+  ...list.map((item) => {
+    const route = {
+      path: `/${item.name}`,
+      component: require(`./pages/${item.name}`),
+    };
+    return route;
+  }),
 ];
 
 const router = new VueRouter({
