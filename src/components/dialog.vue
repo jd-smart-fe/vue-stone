92fd4822ac16775a74f8ff611c377fae23d6821a
tianmeng <tianmeng1@jd.com>
2017-04-24 15:18:08 +0800

    扩展dialog组件到Vue原型上

--- src/components/dialog.vue
+++ src/components/dialog.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-mask :shown="shown" ref="mask">
-    </v-mask>
+      </v-mask>
     <div class="c-dialog" v-show="shown">
       <h4 class="c-dialog-title">{{dialog.title}}</h4>
       <div class="c-dialog-body" :class="[!dialog.description ? 'c-dialog-body-empty':'']">
@@ -12,164 +12,164 @@
       <div class="c-dialog-footer">
         <div class="c-dialog-buttons" :class="getButtonClass()">
           <template v-if="dialog.buttons.length===0">
-            <a href="#"@click.prevent="leftBtn">确定</a>
-            <a href="#"@click.prevent="rightBtn">取消</a>
-          </template >
+              <a href="#"@click.prevent="leftBtn">确定</a>
+              <a href="#"@click.prevent="rightBtn">取消</a>
+            </template >
           <template v-else>
-            <a href="#" v-for="(item,index) in dialog.buttons" @click.prevent="item.defaultCallback">{{item.text}}</a>
+            <a href="#" v-for="(item,index) in dialog.buttons" @click.prevent="item.callback">{{item.text}}</a>
           </template >
-
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
-  export default {
-    name: 'v-dialog',
-    data() {
-      return {
-        modal: false,
-      };
-    },
-    props: {
-      shown: {
-        type: Boolean,
-        required: false,
-        default: false,
-      },
-      options: {
-        type: Object,
-        required: false,
-        default: {},
-      },
-    },
-    computed: {
-      dialog: {
-        get() {
-          const _dialog = this.options;
-          const dialog = {
-            title: _dialog.title || '提示',
-            description: _dialog.description,
 
-            buttons: _dialog.buttons ? _dialog.buttons.map((arr, index) => {
-              const defaultInfoArray = [
-                { text: '取消', callback: this.leftBtn },
-                { text: '确定', callback: this.rightBtn },
-              ];
-              // arr.callback = arr.callback || defaultInfoArray[index].callback;
-              // arr.text = arr.text || defaultInfoArray[index].text;
-              arr.defaultCallback = defaultInfoArray[index].callback;
-              arr.text = arr.text || defaultInfoArray[index].text;
-              return arr;
-            }) : [],
-          };
-          return dialog;
-        },
+const def = {
+  title: '提示',
+  description: '描述',
+  buttons: [
+    { text: '取消', callback: () => { } },
+    { text: '确定', callback: () => { } },
+    // { text: '取消', callback: false },
+    // { text: '确定', callback: false },
+  ],
+};
+export default {
+  name: 'v-dialog',
+  type: 'singleton',
 
-      },
+  data() {
+    return {
+      shown: false,
+      dialog: def,
+    };
+  },
+  // props: {
+  //   shown: {
+  //     type: Boolean,
+  //     required: false,
+  //     default: true,
+  //   },
+
+  // },
+
+  mounted() {
+    // this.$refs.mask.$on('click', () => {
+    //   this.$emit('maskClick');// mask的click事件向上抛
+    // });
+  },
+  methods: {
+    init() {
+      if (!this.inited) {
+        document.body.appendChild(this.$el);
+        this.inited = true;
+      }
     },
-    mounted() {
-      this.$refs.mask.$on('click', () => {
-        this.$emit('maskClick');// mask的click事件向上抛
-      });
+    show(options) {
+      if (!this.inited) {
+        this.init();
+      }
+      this.shown = true;
+      this.dialog = Object.assign(def, options);
+      // this.shown = true;
+      // console.debug(666);
     },
-    methods: {
-      leftBtn() {
-        this.$emit('defaultClick');
-        if (this.dialog.buttons[0] && this.dialog.buttons[0].callback) {
-          this.dialog.buttons[0].callback();
-        }
-      },
-      rightBtn() {
-        this.$emit('primaryClick');
-        if (this.dialog.buttons[1] && this.dialog.buttons[1].callback) {
-          this.dialog.buttons[1].callback();
-        }
-      },
-      getButtonClass() {
-        const buttonLength = this.dialog.buttons.length;
-        let className = 'c-dialog-button-2';
-        switch (buttonLength) {
-          case 1:
-            className = 'c-dialog-button-1';
-            break;
-          default:
-            break;
-        }
-        return className;
-      },
+    hide() {
+      this.shown = false;
     },
-  };
+    getButtonClass() {
+      const buttonLength = this.dialog.buttons.length;
+      let className = 'c-dialog-button-2';
+      switch (buttonLength) {
+        case 1:
+          className = 'c-dialog-button-1';
+          break;
+        default:
+          break;
+      }
+      return className;
+    },
+  },
+};
 </script>
 <style>
-  @import '../styles/default-theme/variables.css';
-  @import '../styles/mixins.css';
-  .c-dialog {
-    width: 2.62rem;
-    max-height: 2rem;
-    position: fixed;
-    z-index: 101;
-    background-color: $white;
-    top: 50%;
-    left: 50%;
-    padding: 0.3rem 0 0.4rem;
-    transform: translateX(-50%) translateY(-50%);
-    box-sizing: border-box;
-    border-radius: 0.02rem;
-    .c-dialog-title {
-      text-align: center;
-      color: $c-dialog-title;
-      margin: 0 0 0.3rem;
-    }
-    > .c-dialog-body {
-      padding-bottom: 0.3rem;
-      >div {
-        max-height: 0.48rem;
-        overflow: auto;
-        > p {
-          color: $gray-light;
-          text-align: center;
-          margin: 0;
-        }
-      }
-    }
-    .c-dialog-footer > .c-dialog-buttons {
-      position: absolute;
-      bottom: 0;
-      width: 100%;
-      height: 0.40rem;
-      line-height: 0.40rem;
-      border-top: 0.01rem $gray-lighter solid;
-      > a {
-        display: block;
-        height: 100%;
-        float: left;
+@import '../styles/default-theme/variables.css';
+@import '../styles/mixins.css';
+.c-dialog {
+  width: 2.62rem;
+  max-height: 2rem;
+  position: fixed;
+  z-index: 101;
+  background-color: $white;
+  top: 50%;
+  left: 50%;
+  padding: 0.3rem 0 0.4rem;
+  transform: translateX(-50%) translateY(-50%);
+  box-sizing: border-box;
+  border-radius: 0.02rem;
+  .c-dialog-title {
+    text-align: center;
+    color: $c-dialog-title;
+    margin: 0 0 0.3rem;
+  }
+  >.c-dialog-body {
+    padding-bottom: 0.3rem;
+    >div {
+      max-height: 0.48rem;
+      overflow: auto;
+      >p {
+        color: $gray-light;
         text-align: center;
-        text-decoration: none;
-        color: $gray;
-      }
-      a+a {
-        border-left: 0.01rem $gray-lighter solid;
+        margin: 0;
       }
     }
   }
-  /**
+  .c-dialog-footer>.c-dialog-buttons {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 0.40rem;
+    line-height: 0.40rem;
+    border-top: 0.01rem $gray-lighter solid;
+    >a {
+      display: block;
+      height: 100%;
+      float: left;
+      text-align: center;
+      text-decoration: none;
+      color: $gray;
+    }
+    a+a {
+      border-left: 0.01rem $gray-lighter solid;
+    }
+  }
+}
+
+
+/**
    *一个按钮样式
    */
-  .c-dialog-button-1 > a {
-    width: 100%;
-  }
-  /**
+
+.c-dialog-button-1>a {
+  width: 100%;
+}
+
+
+/**
    *两个按钮样式
    */
-  .c-dialog-button-2 > a {
-    width: 49.8%;
-  }
-   /**
+
+.c-dialog-button-2>a {
+  width: 49.8%;
+}
+
+
+/**
    *没有text样式
    */
-  .c-dialog > .c-dialog-body-empty{
-    padding: 0 ;
-  }
+
+.c-dialog>.c-dialog-body-empty {
+  padding: 0;
+}
 </style>
--- src/pages/dialog.vue
+++ src/pages/dialog.vue
@@ -10,8 +10,8 @@
       </div>
     </v-panel>
 
-    <v-dialog ref="dialog" :shown="showDialog" :options="dialogOptions">
-    </v-dialog>
+    <!--<v-dialog ref="dialog" :shown="showDialog" :options="dialogOptions">
+    </v-dialog>-->
 
   </div>
 </template>
@@ -24,7 +24,6 @@
     data() {
       return {
         showDialog: false,
-
         dialogOptions: {
           title: '热水器报警',
           description: '热水器过热',
@@ -38,27 +37,44 @@
             },
           ],
         },
-
+        test: '',
       };
     },
 
     mounted() {
 
-      this.$refs.dialog.$on('primaryClick', () => {
-        console.log('触发primaryClick事件');
-        this.showDialog = false;
+      // this.$refs.dialog.$on('primaryClick', () => {
+      //   console.log('触发primaryClick事件');
+      //   this.showDialog = false;
 
-      });
-      this.$refs.dialog.$on('defaultClick', () => {
-        console.log('触发 defaultClick 事件');
-        this.showDialog = false;
-      });
+      // });
+      // this.$refs.dialog.$on('defaultClick', () => {
+      //   console.log('触发 defaultClick 事件');
+      //   this.showDialog = false;
+      // });
     },
 
     methods: {
 
       click() {
-        this.showDialog = true;
+        this.$dialog.show({
+          buttons: [
+            { text: '确定', callback: this.left.bind(this) },
+            { text: '取消', callback: this.right.bind(this) },
+          ],
+          title: 'JDSmart',
+          description: 'No Samrt No Good',
+        });
+      },
+      left() {
+        this.test = 'l';
+        this.$dialog.hide();
+        console.log(this.test);
+      },
+      right() {
+        this.test = 'r';
+        console.log(this.test);
+        this.$dialog.hide();
       },
     },
   };
