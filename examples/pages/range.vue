336ed508657ee98566114f25e522a34ca512e675
hucong <hucong@jd.com>
2017-06-25 16:23:54 +0800

    modify examplr

--- examples/pages/range.vue
+++ examples/pages/range.vue
@@ -1,5 +1,4 @@
 <template>
-
   <div>
     <div class="page-header row-2 u-cross-center">
       <div class="page-header-left u-cross-center row-2">
@@ -12,44 +11,22 @@
     <v-panel>
       <div slot="header" class="c-panel-header row-1 u-cross-center">range - noStep : dots is objects</div>
       <div slot="body" class="c-panel-body row-3 u-cross-center">
-        <v-range slot="main" ref="range"
-          v-model="range_data.value"
-          :min="range_data.min"
-          :max="range_data.max"
-          :tooltip="range_data.tooltip"
-          :is_step="range_data.isStep"
-          :dots="range_data.dots"
-        >
+        <v-range slot="main" ref="range" v-model="range_data_value" :min="range_data.min" :max="range_data.max" :tooltip="range_data.tooltip" :is_step="range_data.isStep" :dots="range_data.dots">
         </v-range>
       </div>
       <div slot="body" class="c-panel-body row-3 u-cross-center">
-        <v-range slot="main" ref="rangeNumber"
-          v-model="range_data_number.value"
-          :min="range_data_number.min"
-          :max="range_data_number.max"
-          :tooltip="range_data_number.tooltip"
-          :is_step="range_data_number.isStep"
-          :dots="range_data_number.dots"
-        >
+        <v-range slot="main" ref="rangeNumber" v-model="range_data_number_value" :min="range_data_number.min" :max="range_data_number.max" :tooltip="range_data_number.tooltip" :is_step="range_data_number.isStep" :dots="range_data_number.dots">
         </v-range>
       </div>
       <div slot="body" class="c-panel-body row-3 u-cross-center">
-        <v-range slot="main" ref="rangeJson"
-          v-model="range_data_json.value"
-          :is_step="range_data_json.isStep"
-          :dots="range_data_json.dots"
-        >
+        <v-range slot="main" ref="rangeObj" v-model="range_data_obj_value" :is_step="range_data_obj.isStep" :dots="range_data_obj.dots">
         </v-range>
       </div>
       <div slot="body" class="c-panel-body">
-          range - disabled
+        range - disabled
       </div>
       <div slot="body" class="c-panel-body row-3 u-cross-center">
-        <v-range slot="main" ref="range"
-          value="20"
-          :dots="range_data.dots"
-          :disabled="true"
-        >
+        <v-range slot="main" value="20" :dots="range_data.dots" :disabled="true">
         </v-range>
       </div>
     </v-panel>
@@ -58,84 +35,83 @@
 </template>
 
 <script>
-  export default {
-    name: 'range',
-    data() {
+export default {
+  name: 'range',
+  data() {
 
-      return {
-
-        range_data: {
-          value: 100,
-          min: 0,
-          max: 1000,
-          tooltip(val) {
-            return `${val}°C`;
-          },
-          dots: [
-            {
-              icon: 'mode-holiday',
-              text: '啊啊',
-            },
-            {
-              icon: 'mode-freeze',
-              text: '高温',
-            },
-          ],
+    return {
+      range_data_value: 100,
+      range_data: {
+        min: 0,
+        max: 1000,
+        tooltip(val) {
+          return `${val}°C`;
         },
-        range_data_number: {
-          value: 500,
-          min: 0,
-          max: 1000,
-          tooltip(val) {
-            return `当前温度：${val}°C`;
+        dots: [
+          {
+            icon: 'mode-holiday',
+            text: '啊啊',
           },
-          dots: [
-            {
-              text: '低温',
-            },
-            {
-              text: '高温',
-            },
-          ],
+          {
+            icon: 'mode-freeze',
+            text: '高温',
+          },
+        ],
+      },
+      range_data_number_value: 500,
+      range_data_number: {
+        min: 0,
+        max: 1000,
+        tooltip(val) {
+          return `当前温度：${val}°C`;
         },
-        range_data_json: {
-          value: {
+        dots: [
+          {
+            text: '低温',
+          },
+          {
+            text: '高温',
+          },
+        ],
+      },
+      range_data_obj_value: {
+        value: 30,
+        text: '高档',
+      },
+      range_data_obj: {
+        isStep: true,
+        dots: [
+          {
+            value: 10,
+            text: '低档',
+          },
+          {
+            value: 20,
+            text: '中档',
+          },
+          {
             value: 30,
             text: '高档',
           },
-          isStep: true,
-          dots: [
-            {
-              value: 10,
-              text: '低档',
-            },
-            {
-              value: 20,
-              text: '中档',
-            },
-            {
-              value: 30,
-              text: '高档',
-            },
-          ],
-        },
-      };
-    },
+        ],
+      },
+    };
+  },
 
-    mounted() {
-      this.$refs.rangeNumber.$on('change', (value) => {
-        console.log(`当前滑杆的值: ${value}`);
-      });
+  mounted() {
+    this.$refs.range.$on('change', (value) => {
+      console.log(`当前滑杆的值: ${value}`);
+    });
 
-      this.$refs.range.$on('change', (value) => {
-        console.log(`当前滑杆的值: ${value}`);
-      });
+    this.$refs.rangeNumber.$on('change', (value) => {
+      console.log(`当前滑杆的值: ${value}`);
+    });
 
-      this.$refs.rangeJson.$on('change', (value) => {
-        console.log(`当前滑杆的值: ${value.text}`);
-      });
-    },
-  };
+    this.$refs.rangeObj.$on('change', (value) => {
+      console.log(`当前滑杆的值: ${value.text}`);
+    });
+  },
+};
 </script>
 
 <style>
