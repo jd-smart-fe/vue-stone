44884a7f0d1475b5feef1a4f06cdcc27ad47902f
杨立坤 <yanglikun@yanglikundeMacBook-Pro.local>
2017-09-05 17:43:01 +0800

    修改mode-auto\mode-dry无法调用问题

--- examples/pages/icons.vue
+++ examples/pages/icons.vue
@@ -10,6 +10,8 @@
         <span class='icon icon-quick-wash-30'></span>
         <span class='icon icon-quick-wash-15'></span>
         <span class='icon icon-washing-liquid'></span>
+        <span class='icon icon-mode-auto'></span>
+        <span class='icon icon-mode-dry'></span>
         ...
       </div>
     </v-panel>
