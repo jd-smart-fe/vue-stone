e1cf370fef1eb08b763396e4b31cb376b435cda5
hucong <hucong@jd.com>
2018-04-12 15:35:22 +0800

    fix: counter disabled 时误触发事件

--- src/components/counter.vue
+++ src/components/counter.vue
@@ -23,130 +23,135 @@
 </template>
 
 <script>
-export default {
-  name: 'v-counter',
+  export default {
+    name: 'v-counter',
 
-  data() {
-    return {
-      incDisabled: false,
-      decDisabled: false,
-    };
-  },
+    data() {
+      return {
+        incDisabled: false,
+        decDisabled: false,
+      };
+    },
 
-  props: {
-    value: {
-      type: Number,
-      required: false,
-      default: 0,
+    props: {
+      value: {
+        type: Number,
+        required: false,
+        default: 0,
+      },
+      max: {
+        type: Number,
+        default: Infinity,
+      },
+      min: {
+        type: Number,
+        default: -Infinity,
+      },
+      step: {
+        type: Number,
+        required: false,
+        default: 1,
+      },
+      longTap: {
+        type: Boolean,
+        required: false,
+        default: false,
+      },
     },
-    max: {
-      type: Number,
-      default: Infinity,
-    },
-    min: {
-      type: Number,
-      default: -Infinity,
-    },
-    step: {
-      type: Number,
-      required: false,
-      default: 1,
-    },
-    longTap: {
-      type: Boolean,
-      required: false,
-      default: false,
-    },
-  },
 
-  created() {
-    // 验证数字区间是否正确
-    function verification(current, max, min) {
-      if (current > max || current < min) {
-        console.error('The current number must be between the max value and min value');
-        return false;
+    created() {
+      // 验证数字区间是否正确
+      function verification(current, max, min) {
+        if (current > max || current < min) {
+          console.error('The current number must be between the max value and min value');
+          return false;
+        }
+
+        // 通过验证 返回 true。
+        return true;
       }
 
-      // 通过验证 返回 true。
-      return true;
-    }
+      // 验证传入数字是否正确
+      verification(this.current, this.max, this.min);
 
-    // 验证传入数字是否正确
-    verification(this.current, this.max, this.min);
-
-    if (this.value >= this.max) {
-      this.incDisabled = true;
-    }
-    if (this.value <= this.min) {
-      this.decDisabled = true;
-    }
-  },
-
-  watch: {
-    value(val) {
-      if (val >= this.max) {
+      if (this.value >= this.max) {
         this.incDisabled = true;
-        return;
       }
-      if (val <= this.min) {
+      if (this.value <= this.min) {
         this.decDisabled = true;
-        return;
-      }
-
-      if (this.incDisabled) {
-        this.incDisabled = false;
-      }
-      if (this.decDisabled) {
-        this.decDisabled = false;
       }
     },
-  },
 
-  methods: {
-    incHandle() {
-      let value = this.value + this.step;
-      if (value >= this.max) {
-        value = this.max;
-      }
+    watch: {
+      value(val) {
+        if (val >= this.max) {
+          this.incDisabled = true;
+          return;
+        }
+        if (val <= this.min) {
+          this.decDisabled = true;
+          return;
+        }
 
-      this.$emit('increase', value);
-      this.$emit('change', value);
-      this.$emit('input', value);
+        if (this.incDisabled) {
+          this.incDisabled = false;
+        }
+        if (this.decDisabled) {
+          this.decDisabled = false;
+        }
+      },
     },
-    decHandle() {
-      let value = this.value - this.step;
-      if (value <= this.min) {
-        value = this.min;
-      }
 
-      this.$emit('decrease', value);
-      this.$emit('change', value);
-      this.$emit('input', value);
+    methods: {
+      incHandle() {
+        let value = this.value + this.step;
+        if (value > this.max) {
+          value = this.max;
+          return;
+        }
+
+        this.$emit('increase', value);
+        this.$emit('change', value);
+        this.$emit('input', value); // v-model 双向绑定
+      },
+      decHandle() {
+        let value = this.value - this.step;
+        if (value < this.min) {
+          value = this.min;
+          return;
+        }
+
+        this.$emit('decrease', value);
+        this.$emit('change', value);
+        this.$emit('input', value); // v-model 双向绑定
+      },
     },
-  },
-};
+  };
 </script>
 
-<style>
-$width: 0.6rem;
-$height: 0.38rem;
-$icon-size: 0.16rem;
-$left-icon-left-margin: 0.04rem;
-$right-icon-right-margin: 0.04rem;
+<style lang="postcss">
+  $width: 0.6rem;
+  $height: 0.38rem;
+  $icon-size: 0.16rem;
+  $left-icon-left-margin: 0.04rem;
+  $right-icon-right-margin: 0.04rem;
 
 
-.c-counter-dec.c-counter-left-border {
-  border-left-color: #59B8FC;
-}
-.c-counter button.c-btn-icon-only {
-  width: $width;
-  height: $height;
-  font-size: $icon-size;
-}
-.c-counter-inc .c-btn-icon {
-  margin-left: $left-icon-left-margin
-}
-.c-counter-dec .c-btn-icon {
-  margin-right: $right-icon-right-margin
-}
+  .c-counter-dec.c-counter-left-border {
+    border-left-color: #59B8FC;
+  }
+
+  .c-counter button.c-btn-icon-only {
+    width: $width;
+    height: $height;
+    font-size: $icon-size;
+  }
+
+  .c-counter-inc .c-btn-icon {
+    margin-left: $left-icon-left-margin
+  }
+
+  .c-counter-dec .c-btn-icon {
+    margin-right: $right-icon-right-margin
+  }
 </style>
