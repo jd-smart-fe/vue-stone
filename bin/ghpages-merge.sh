902cefa7d805c0f1a213d0f76c6304797f78aa66
pspgbhu <brotherchun001@gmail.com>
2018-04-25 22:39:41 +0800

    chore: 修改了构建细节

--- bin/ghpages-merge.sh
+++ bin/ghpages-merge.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
 git checkout gh-pages
-git merge -Xtheirs master -m "Merge master into gh-pages"
+git merge -Xtheirs master -m "Merge branch master into branch gh-pages"
 git push
 git checkout master
