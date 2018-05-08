1d23c5d5c42bddf99a95743ec10047d68bf164d1
pspgbhu <brotherchun001@gmail.com>
2018-05-08 12:08:02 +0800

    fix: transition

--- src/components/_style_transition.vue
+++ src/components/_style_transition.vue
@@ -4,10 +4,11 @@
 
 <style>
   .fade-enter-active, .fade-leave-active  {
-    transition: opacity .3s ease;
+    opacity: 1;
+    transition: opacity 5s ease;
   }
 
-  .fade-enter, .fade-leave-active {
+  .fade-enter, .fade-leave-to {
     opacity: 0;
   }
 </style>
