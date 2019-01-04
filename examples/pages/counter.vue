ad23347ba5d752faa72bdf4fbf511efd4d32b65e
yanglikun <307474614@qq.com>
2019-01-04 18:14:49 +0800

    fix: counter添加禁用属性；timepicker修改wiki去掉3D效果属性设置

--- examples/pages/counter.vue
+++ examples/pages/counter.vue
@@ -1,18 +1,30 @@
 <template>
-
   <div>
     <tmpl-back></tmpl-back>
-
     <v-panel>
       <div slot="body" class="c-panel-body row-3 u-cross-center">
         <div slot="title" class="c-panel-title ">counter:
-          <div class="number-text">
+          <div class="number-text" >
             {{ countNumber }}
           </div>
         </div>
         <v-counter
           slot="main" ref="counter" v-model="countNumber"
           :max="10" :min="0" :step="1" :longTap="true">
+        </v-counter>
+      </div>
+    </v-panel>
+
+    <v-panel>
+      <div slot="body" class="c-panel-body row-3 u-cross-center">
+        <div slot="title" class="c-panel-title ">禁用状态:
+          <div :class="['number-text', disabled ? 'disabled' : '']">
+            {{ countNumber2 }}
+          </div>
+        </div>
+        <v-counter
+          slot="main" ref="counterDisabled" v-model="countNumber2"
+          :max="max" :min="min" :step="step" :disabled= "disabled" :longTap="true">
         </v-counter>
       </div>
     </v-panel>
@@ -28,11 +40,15 @@
     data() {
       return {
         countNumber: 5,
+        disabled: true,
+        countNumber2: 5,
+        max: 10,
+        min: 0,
+        step: 1,
       };
     },
 
     mounted() {
-
       this.$refs.counter.$on('increase', (val) => {
         console.log(`触发increase事件 当前值为：${val}`);
       });
@@ -56,5 +72,7 @@
     font-size: $font-size-32;
     color: $c-primary;
   }
-
+  .disabled {
+   color: #ccc;
+  }
 </style>
